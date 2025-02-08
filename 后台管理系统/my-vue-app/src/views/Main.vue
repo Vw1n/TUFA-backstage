a485c2af4e4083239f23acd077b23ed51ad04766
Vw1n <2374150718@qq.com>
2025-02-08 21:18:59 +0800

    first commit

---
+++ 后台管理系统/my-vue-app/src/views/Main.vue
@@ -0,0 +1,30 @@
+<script setup>
+import CommonAside from '@/components/CommonAside.vue'
+</script>
+
+<template>
+    <div class="common-layout">
+        <el-container class="lay-container">
+            <!-- 自定义左侧的组件 -->
+            <common-aside />
+            <el-container>
+                <el-header>
+                    <common-header />
+                </el-header>
+                <el-main class="right-main">
+                    main
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<style scoped>
+.common-layout,
+.lay-container {
+    height: 100%;
+}
+.el-header{
+    background-color: #333;
+}
+</style>
